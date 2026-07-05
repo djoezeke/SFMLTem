b7ad67c54b4c42aff66275a4bb53b30130eb9e52
Sackey Ezekiel Etrue <sackeyetrue@gmail.com>
2026-07-05 02:57:53 +0000

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,26 @@
-#include "imgui.h" // necessary for ImGui::*, imgui-SFML.h doesn't include imgui.h
-
-#include "imgui-SFML.h" // for ImGui::SFML::* functions and SFML-specific overloads
-
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode({640, 480}), "ImGui + SFML = <3");
-	window.setFramerateLimit(60);
-	if (!ImGui::SFML::Init(window))
-		return -1;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Template");
 
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition(200.f, 150.f);
 
-	sf::Clock deltaClock;
-	while (window.isOpen())
-	{
-		while (const auto event = window.pollEvent())
-		{
-			ImGui::SFML::ProcessEvent(window, *event);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-			if (event->is<sf::Event::Closed>())
-			{
-				window.close();
-			}
-		}
+        window.clear(sf::Color::Black);
+        window.draw(shape);
+        window.display();
+    }
 
-		ImGui::SFML::Update(window, deltaClock.restart());
-
-		ImGui::ShowDemoWindow();
-
-		ImGui::Begin("Hello, world!");
-		ImGui::Button("Look at this pretty button");
-		ImGui::End();
-
-		window.clear();
-		window.draw(shape);
-		ImGui::SFML::Render(window);
-		window.display();
-	}
-
-	ImGui::SFML::Shutdown();
+    return 0;
 }
